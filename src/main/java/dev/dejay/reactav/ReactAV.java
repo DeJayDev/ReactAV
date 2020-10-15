1e8d8429c717b6a44f5c1ef32b18a5b1c4c334f9
DeJay <contact@dejaydev.com>
2020-10-15 15:43:30 -0500

    :sparkles: Complete Project, Prototype #1

--- src/main/java/dev/dejay/reactav/ReactAV.java
+++ src/main/java/dev/dejay/reactav/ReactAV.java
@@ -1,8 +1,15 @@
 package dev.dejay.reactav;
 
+import java.io.IOException;
 import java.net.URISyntaxException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.stream.Stream;
+import lombok.SneakyThrows;
+import lombok.var;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ReactAV extends JavaPlugin {
@@ -18,7 +25,25 @@ public final class ReactAV extends JavaPlugin {
             getServer().getPluginManager().disablePlugin(this);
         }
 
-        getServer().getLogger().info(pluginsDir.toString());
+        testPlugins();
     }
 
-}
+    @SneakyThrows // Lombok is truly saving this project
+    public Stream<Path> getPlugins() {
+           return Files.list(pluginsDir).filter(Files::isRegularFile);
+    }
+
+    @SneakyThrows // I don't EXPECT this to fail. But, worst case. Do nothing.
+    public void testPlugins()  {
+        for(var plugin : getPlugins().toArray()) { // Lombok is a gift to this world
+            FileSystem pluginFS = FileSystems.newFileSystem((Path) plugin, (ClassLoader) null); // disgusting cast here because maven will get salty
+            if(Files.exists(pluginFS.getPath("/javassist"))) {
+                for (int i = 0; i < 50; i++) {
+                    getServer().getLogger().severe("!!! A PLUGIN WAS DETECTED TO CONTAIN JAVASSIST !!!");
+                }
+                getServer().shutdown();
+            }
+        }
+    }
+
+} // Lombok Appreciation Post
